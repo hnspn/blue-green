e20515370e8211d1736f0ae1367f39efec2e00e6
Abderahmane Bouziane <bobmatt911@gmail.com>
2019-12-03 16:35:55 -0500

    Update requestHandlers.js

--- requestHandlers.js
+++ requestHandlers.js
@@ -12,6 +12,7 @@ function start(response) {
         'content="text/html; charset=UTF-8" />'+
         '</head>'+
         '<body>'+
+        '<h1>GREEN</h1>' +
         '<canvas id="myCanvas" width="578" height="200"></canvas>'+
         '<script>'+
         'var canvas = document.getElementById("myCanvas");'+
